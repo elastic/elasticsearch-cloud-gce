96c40d08ec782d337c5e1be0d434597308c0800d
tlrx <tlrx.dev@gmail.com>
2014-11-05 11:29:31 +0100

    Removing unused imports

--- src/main/java/org/elasticsearch/plugin/cloud/gce/CloudGcePlugin.java
+++ src/main/java/org/elasticsearch/plugin/cloud/gce/CloudGcePlugin.java
@@ -23,8 +23,6 @@ import org.elasticsearch.cloud.gce.GceModule;
 import org.elasticsearch.common.collect.Lists;
 import org.elasticsearch.common.component.LifecycleComponent;
 import org.elasticsearch.common.inject.Module;
-import org.elasticsearch.common.logging.ESLogger;
-import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.plugins.AbstractPlugin;
 import org.elasticsearch.repositories.RepositoriesModule;
